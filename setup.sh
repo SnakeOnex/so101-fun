72dbac605a6f11035abe128bbfe6351d4755a107
snakeonex <sheeproman@gmail.com>
2026-02-01 22:20:24 +0000

    updated setup and reqs for batch camera rendering in genesis

--- setup.sh
+++ setup.sh
@@ -2,3 +2,4 @@ curl -LsSf https://astral.sh/uv/install.sh | sh
 apt-get update -y
 apt-get install -y   libxrender1 libxext6 libx11-6 libxi6 libxrandr2 libxcursor1 libxinerama1   libgl1-mesa-glx libglib2.0-0
 apt-get install -y libegl1 libegl1-mesa libgl1 libgl1-mesa-dri libglvnd0 mesa-utils-extra
+apt-get install -y libvulkan1 vulkan-tools
